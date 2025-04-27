a1b2998d9e3265bb651dea46e6022ffe16934212
jojojoelelel <JoelHFJ7643@gmail.com>
2025-04-27 18:58:20 +0800

    local version 1.0

--- elastic-backend/cv-index.py
+++ elastic-backend/cv-index.py
@@ -1,19 +1,18 @@
-import pandas as pd
 import uuid
-import numpy as np
 from elasticsearch import Elasticsearch
-from tqdm import tqdm
+# from tqdm import tqdm
 
-csv_path = 'cv-valid-dev.csv'
-df = pd.read_csv(csv_path)
-df = df.replace(np.nan, None)
+es = Elasticsearch(
+    "http://localhost:9200",
+)
 
-es = Elasticsearch("http://localhost:9200")
-
-if not es.ping():
-    raise ValueError('Connection to Elasticsearch failed')
-else:
-    print("Connected to Elasticsearch!")
+try:
+    if es.ping():
+        print("Successfully connected to Elasticsearch!")
+    else:
+        print("Failed to connect to Elasticsearch.")
+except Exception as e:
+    print(f"Error: {e}")
 
 index_name = 'cv-transcriptions'
 
@@ -21,6 +20,7 @@ if not es.indices.exists(index=index_name):
     es.indices.create(index=index_name, body={
         'mappings': {
             'properties': {
+                'filename': { 'type': 'text' },
                 'generated_text': {'type': 'text'},
                 'duration': {'type': 'float'},
                 'age': {'type': 'keyword'},
@@ -30,16 +30,44 @@ if not es.indices.exists(index=index_name):
         }
     })
 
-for _, row in tqdm(df.iterrows(), total=len(df)):
-    doc = {
-        "generated_text": row.get("generated_text"),
-        "duration": row.get("duration"),
-        "age": row.get("age"),
-        "gender": row.get("gender"),
-        "accent": row.get("accent")
-    }
-    es.index(index=index_name, id=str(uuid.uuid4()), body=doc)
+csv_path = 'cv-valid-dev.csv'
+
+# pandas and numpy
+
+# import pandas as pd
+# import numpy as np
+
+# df = pd.read_csv(csv_path)
+# df = df.replace(np.nan, None)
+
+# for _, row in tqdm(df.iterrows(), total=len(df)):
+#     doc = {
+#         "generated_text": row.get("generated_text"),
+#         "duration": row.get("duration"),
+#         "age": row.get("age"),
+#         "gender": row.get("gender"),
+#         "accent": row.get("accent")
+#     }
+#     es.index(index=index_name, id=str(uuid.uuid4()), body=doc)
+
+# print('Indexing complete')
+
+# python csv
+
+import csv
+
+with open(csv_path, mode='r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        doc = {
+            "filename": row.get("filename").split("/")[1] or None,
+            "generated_text": row.get("generated_text") or None,
+            "duration": float(row["duration"]) if row.get("duration") else None,
+            "age": row.get("age") or None,
+            "gender": row.get("gender") or None,
+            "accent": row.get("accent") or None
+        }
+        es.index(index=index_name, id=str(uuid.uuid4()), body=doc)
 
 print('Indexing complete')
-
 
