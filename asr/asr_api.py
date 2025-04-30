d91c4f19e4613e9b20e5c2bde6f34822ec97ab41
jojojoelelel <JoelHFJ7643@gmail.com>
2025-04-30 14:00:45 +0800

    cleaning up

--- asr/asr_api.py
+++ asr/asr_api.py
@@ -1,3 +1,4 @@
+# Import libraries
 from flask import Flask, request, jsonify
 
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
@@ -8,58 +9,75 @@ from pydub import AudioSegment
 
 import os
 
+# Initialise Flask App
 app = Flask(__name__)
 
 # load model and processor
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
 
+# Enable GPU if available and not using torch+cpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
+# Ping Endpoint for testing
 @app.route('/ping', methods=['GET'])
 def ping():
     return "pong", 200
-     
+
+# ASR Endpoint
 @app.route('/asr', methods=['POST'])
 def asr():
+    # Check if file is given
     if 'file' not in request.files:
         return jsonify({'error': 'No file given'}), 400
     
+    # Get file
     file = request.files['file']
 
+    # Check if file is empty
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
+    
     try:
+        # Create temporary mp3 file path
         temp_file_path = 'temp_audio.mp3'
+        # Save input file as temporary mp3 file
         file.save(temp_file_path)
 
+        # Convert temporary mp3 file to wav for processing
         audio = AudioSegment.from_mp3(temp_file_path)
         wav_file_path = 'temp_audio.wav'
         audio.export(wav_file_path, format='wav')
 
+        # Load wav file and resample at 16000 Hz
         audio_input, _ = librosa.load(wav_file_path, sr=16000)
 
+        # Run transcription
         input_values = processor(audio_input, return_tensors='pt').input_values.to(device)
         with torch.no_grad():
             logits = model(input_values).logits
-        
         predicted_ids = torch.argmax(logits, dim=-1)
         transcription = processor.decode(predicted_ids[0])
 
+        # Obtain duration of audio file
         duration = librosa.get_duration(y=audio_input, sr=16000)
 
+        # Remove temporary files
         os.remove(temp_file_path)
         os.remove(wav_file_path)
 
+        # Return transcription and duration
         return jsonify({
             'transcription': transcription,
             'duration': str(duration)
         }), 200
     
+    # Error handling
     except Exception as e:
         print(f"Error processing the file: {e}")
         return jsonify({'error': str(e)}), 500
-
+    
+# Run Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
--- asr/cv-decode.py
+++ asr/cv-decode.py
@@ -1,8 +1,10 @@
+# Import Libraries
 import requests
 import os
 import csv
 from tqdm import tqdm
 
+# Folder paths
 input_folder = 'input'
 output_folder = 'output'
 
@@ -11,14 +13,19 @@ output_csv_file = os.path.join(output_folder, 'cv-valid-dev.csv')
 
 api_url = 'http://localhost:8001/asr'
 
+# List to store updated records
 updated_rows = []
 
+# Open input csv file
 with open(input_csv_file, mode='r', newline='', encoding='utf-8') as infile:
     reader = csv.DictReader(infile)
+    # Add generated_text column to field names
     fieldnames = reader.fieldnames + ['generated_text']
+    # Iterate over records in input csv file
     for row in tqdm(reader):
-        file_path = os.path.join(row['filename'])
-
+        # Get file path
+        file_path = row['filename']
+        # Call ASR API
         try:
             with open(file_path, 'rb') as f:
                 response = requests.post(api_url, files={'file': f})
@@ -31,11 +38,17 @@ with open(input_csv_file, mode='r', newline='', encoding='utf-8') as infile:
             row['generated_text'] = ''
             row['duration'] = ''
 
+        # Append updated record to list
         updated_rows.append(row)
 
+# Open output csv file
 with open(output_csv_file, mode='w', newline='', encoding='utf-8') as outfile:
+    # Create writer object
     writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    # Write header
     writer.writeheader()
+    # Write list into output csv file
     writer.writerows(updated_rows)
 
+# Acknowledgement
 print("Transcriptions saved.")
